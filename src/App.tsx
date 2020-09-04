cb05db82c7cf71d6bc4fc1fbd30057b2399e5e68
Anderson Viana <andiana@hotmnai.com>
2020-09-03 23:22:27 -0300

    configured and added global style on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <div>Learn React</div>;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello World!!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
