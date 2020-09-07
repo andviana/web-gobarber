b83edab3867b86d0ee0a454b7c558343d6e5ff84
Anderson Viana <andiana@hotmnai.com>
2020-09-07 05:09:44 -0300

    added skeleton for page dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <p>Teste</p>
+      <Container>
+        <h1>DashBoard</h1>
+      </Container>
+    </div>
+  );
+};
+export default Dashboard;
