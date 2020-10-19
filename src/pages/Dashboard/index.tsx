b2efef91b77c4eccf9621fbc59141734bcdb3385
Anderson Viana <andiana@hotmnai.com>
2020-10-19 10:59:43 -0300

    adicionando elementos graficos e estilização do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,87 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectdDate, setSelectedDate] = useState(new Date());
+
+  const { signOut, user } = useAuth();
   return (
-    <div>
-      <p>Teste</p>
-      <Container>
-        <h1>DashBoard</h1>
-      </Container>
-    </div>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/10578253?s=460&u=cac27daa1c6c97dd92fe760221e7f48614a94691&v=4"
+                alt="Anderson Viana"
+              />
+              <strong>Anderson Viana</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/10578253?s=460&u=cac27daa1c6c97dd92fe760221e7f48614a94691&v=4"
+                  alt="Anderson Viana"
+                />
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
   );
 };
 export default Dashboard;
